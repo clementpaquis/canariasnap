aa93e54ad21661f6a44284ecf9e8e2ba6de84c39
clementpaquis <clementpaquis@gmail.com>
2019-05-10 12:02:46 +0100

    first commit

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,49 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-camera.Camera",
+      "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "Camera"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.CameraPopoverOptions",
+      "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "CameraPopoverOptions"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.camera",
+      "file": "plugins/cordova-plugin-camera/www/Camera.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "navigator.camera"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.CameraPopoverHandle",
+      "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "CameraPopoverHandle"
+      ]
+    },
+    {
+      "id": "cordova.custom.plugins.exitapp.exitApp",
+      "file": "plugins/cordova.custom.plugins.exitapp/www/ExitApp.js",
+      "pluginId": "cordova.custom.plugins.exitapp",
+      "merges": [
+        "navigator.app"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-camera": "4.0.3",
+    "cordova.custom.plugins.exitapp": "1.0.0"
+  };
+});
---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+};
+
+//binding camera button
+function getCameraStream(){
+    document.getElementById('cam-btn').addEventListener('click',takePicture);
+}
+
+//binding return button
+document.getElementById('back-btn').addEventListener('click',function(){
+    var res = confirm("Leave Canaria Snap ?");
+    if(res){
+        navigator.app.exitApp();
+    }
+});
+
+//get camera stream and take picture
+function takePicture(){
+    navigator.camera.getPicture(success,error,{
+        quality:50,
+        correctOrientation: true,
+        destinationType:Camera.DestinationType.DATA_URL,
+    });
+}
+
+//shows image taken by takePicture() in dom
+function success(imageData){
+    var img = document.getElementById('snap');
+    img.src = "data:image/jpeg;base64,"+imageData;
+    document.getElementById('snap').style.display='block';
+}
+
+//if an error occurs with takePicture()
+function error(err){
+    console.log('error : ' + err);
+}
+
+this.getCameraStream();
+ 
+app.initialize();
